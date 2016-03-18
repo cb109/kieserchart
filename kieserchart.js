cde451bb26dbf55fa25c2a2b735d382beb24e95e
Christoph Buelter <c.buelter@arcor.de>
2016-03-19 00:00:38 +0100

    Add docstrings.

--- kieserchart.js
+++ kieserchart.js
@@ -1,16 +1,24 @@
 var kieserchart = (function() {
   'use strict';
 
-  // Input file-pick triggers parsing + drawing.
+  var dateFormat = "DD.MM.YYYY";
+
+  /* Input file-pick triggers parsing and drawing. */
   $(document).ready(function(){
     $("#csv-file").change(handleFileSelect);
   });
 
+  /* Let the user pick a file when the button is pressed. */
   function handleFileSelect(evt) {
     var file = evt.target.files[0];
     parseCSV(file);
   }
 
+  /* Parse the given .csv file into a list of objects.
+   *
+   * Each object represents a row, with each column header
+   * as a key and the respective cell content as a value.
+   */
   function parseCSV(csvFile) {
     var config = {
       header: true,
@@ -24,13 +32,20 @@ var kieserchart = (function() {
     Papa.parse(csvFile, config);
   }
 
+  /* Transform the parsed CSV data into a representation for the chart.
+   *
+   * The chart expects a certain data schema; this is wrapped in Value
+   * and DataLine objects here. The date column is parsed and later used
+   * to inject dates into each data row, so it can be display on the
+   * x-axis.
+   */
   function transform(parsed) {
     // Collect column headers.
     var columnHeaders = [];
     var firstRow = parsed.data[0];
     Object.keys(firstRow).forEach(function(key, index) {
       // Collect only lbs, not sec.
-      var invalid = key.indexOf("sec") > -1;
+      var invalid = key.indexOf('sec') > -1;
       if (!invalid) {
         columnHeaders.push(key);
       }
@@ -65,20 +80,24 @@ var kieserchart = (function() {
     drawGraph(valueLines);
   }
 
+  /* A chart data sample. */
   function Value(x, y) {
     this.x = x;
     this.y = y;
   }
 
+  /* A data line for the chart.
+   *
+   * The values list holds Value objects.
+   */
   function DataLine(key, values) {
     this.key = key;
     this.values = values;
     // this.area = true;
   }
 
+  /* Construct and draw the nv3d chart */
   function drawGraph(valueLines) {
-    var dateFormat = "DD.MM.YYYY";
-
     nv.addGraph(function() {
       var chart = nv.models.lineChart()
         .margin({left: 50, right: 50, top: 50})
